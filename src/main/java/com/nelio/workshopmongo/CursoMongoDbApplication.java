bab3b302bdb11bc135bde738c7c8ad1e973d475b
ec-ayrton <ec.ayrton@gmail.com>
2020-09-05 16:57:53 -0300

    conectando mongodb com repository e service

--- src/main/java/com/nelio/workshopmongo/CursoMongoDbApplication.java
+++ src/main/java/com/nelio/workshopmongo/CursoMongoDbApplication.java
@@ -1,4 +1,4 @@
-package com.nelio.course;
+package com.nelio.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
