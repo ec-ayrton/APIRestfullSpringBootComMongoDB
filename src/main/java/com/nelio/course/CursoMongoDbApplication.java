44bac000760693dae07309a602fbfb326f43e746
ec-ayrton <ec.ayrton@gmail.com>
2020-09-05 15:57:29 -0300

    first commit

---
+++ src/main/java/com/nelio/course/CursoMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.nelio.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelio/course/CursoMongoDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nelio.course;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoMongoDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
