ab17485978cc78583bceec09d707857f6da4cf36
ec-ayrton <ec.ayrton@gmail.com>
2020-09-05 21:59:03 -0300

    EntityPost com user aninhado

--- src/main/java/com/nelioalves/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package com.nelioalves.workshopmongo.repositories;
+package com.nelioalves.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
